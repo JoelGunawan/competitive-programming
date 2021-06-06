eb16aea9b109230cbdaecdb70fa542548dcce77a
Joel Gunawan <joelgun14@gmail.com>
2021-06-06 12:42:16 +0700

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# NOTHING TO SEE HERE
-REALLY NOTHING
+# Accounts
+Codeforces: https://codeforces.com/profile/joelgun14
+AtCoder: https://atcoder.jp/users/joelgun14
+TLX: https://tlx.toki.id/profiles/joelgunawan
+CodeChef: https://www.codechef.com/users/joelgunawan
---
+++ template.cpp
@@ -0,0 +1,22 @@
+// header file
+#include <bits/stdc++.h>
+// pragma
+#pragma GCC target("avx2")
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
+// macros
+#define endl "\n"
+#define ll long long
+#define mp(a, b) make_pair(a, b)
+#define pb(a) push_back(a)
+#define pf(a) push_front(a)
+#define ins(a) insert(a)
+
+using namespace std;
+
+int main()
+{
+    // fast io using cin/cout
+    ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+    return 0;
+}
