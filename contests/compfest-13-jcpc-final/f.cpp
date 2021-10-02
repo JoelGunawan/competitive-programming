f15e11ee6a45ac9f647102e6c96bccd21563c5aa
Joel Gunawan <joelgun14@gmail.com>
2021-10-02 10:07:44 +0700

    pre contest template added

---
+++ contests/compfest-13-jcpc-final/f.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define ll long long
+#define lll __int128
+#define pb push_back
+#define ins insert
+#define ull unsigned long long
+#define ullll unsigned __int128
+#define pointer(it) *it
+#define lb lower_bound
+#define ub upper_bound
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    cout << fixed << setprecision(10);
+    return 0;
+}
