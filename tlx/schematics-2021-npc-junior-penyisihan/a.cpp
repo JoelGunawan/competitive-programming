bac8137f62d31334feb729701121da9ac9c0ed76
Joel Gunawan <joelgun14@gmail.com>
2021-10-03 21:22:59 +0700

    full solved schematics-npc-junior-2021-penyisihan A

--- tlx/schematics-2021-npc-junior-penyisihan/a.cpp
+++ tlx/schematics-2021-npc-junior-penyisihan/a.cpp
@@ -4,15 +4,19 @@
 #define pb push_back
 using namespace std;
 int dp[1001][1001];
+int mod = 1e9 + 7;
 void filldp()
 {
+    memset(dp, 0, sizeof(dp));
     for(int i = 0; i < 1001; ++i)
-        dp[i][0] = 1, dp[0][i] = 1;
+        dp[0][i] = 1;
     for(int i = 1; i < 1001; ++i)
     {
         for(int j = 1; j < 1001; ++j)
         { 
-            
+            dp[i][j] = dp[i][j - 1];
+            if(i >= j)
+                dp[i][j] = (dp[i][j] + dp[i - j][j]) % mod;
         }
     }
 }
@@ -20,11 +24,11 @@ void solve()
 {
     int m, n;
     cin >> m >> n;
-
-
+    cout << dp[m][n] << endl;
 }
 int main()
 {
+    filldp();
     int t;
     cin >> t;
     while(t--)
