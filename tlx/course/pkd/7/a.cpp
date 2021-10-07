84a3d70cc090704a4a0ef4dab6ae6d8690b78678
Joel Gunawan <joelgun14@gmail.com>
2021-10-07 08:09:52 +0700

    added course pkd 7A

---
+++ tlx/course/pkd/7/a.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int a[n];
+    for(int i = 0; i < n; ++i)
+        cin >> a[i];
+    int k;
+    cin >> k;
+    int dp[k + 1];
+    fill(dp, dp + k + 1, 1e9); dp[0] = 0;
+    for(int i = 1; i <= k; ++i)
+    {
+        for(int j = 0; j < n; ++j)
+        {
+            if(a[j] <= i)
+                dp[i] = min(dp[i - a[j]] + 1, dp[i]);
+        }
+    }
+    if(dp[k] == 1e9)
+        cout << -1 << endl;
+    else
+        cout << dp[k] << endl;
+    return 0;
+}
