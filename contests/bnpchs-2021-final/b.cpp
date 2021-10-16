c203725ac859a529971d3771a8aab884d951b07f
Joel Gunawan <joelgun14@gmail.com>
2021-10-16 08:54:26 +0700

    added pre contest template for bnpchs 2021 finals

---
+++ contests/bnpchs-2021-final/b.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define pb push_back
+#define ins insert
+#define pf push_front
+#define pq priority_queue
+#define ll long long
+#define lll long long long
+#define lb lower_bound
+#define ub upper_bound
+#define fast_io() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+using namespace std;
+int main()
+{
+    fast_io();
+    
+    return 0;
+}
