f3fefb5fdaae87a5909eee7aab4f9f80aa857dee
Joel Gunawan <joelgun14@gmail.com>
2021-12-02 10:50:17 +0700

    added template for practice sessions

---
+++ tlx/pelatnas-2021/pelatnas-1/template.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define pb push_back
+#define ins isnert
+#define fi first
+#define se second
+#define ll long long
+#define endl "\n"
+#define ld long double
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    
+    return 0;
+}
