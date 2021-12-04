a00c3b886a4c32d48095ec7c1928598d038de428
Joel Gunawan <joelgun14@gmail.com>
2021-12-04 10:07:30 +0700

    added SPOJ TEST

---
+++ spoj/TEST.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a;
+    while(cin >> a)
+    {
+        if(a == 42)
+            break;
+        else
+            cout << a << endl;
+    }
+    return 0;
+}
