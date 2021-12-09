04a655ebe76550892c6a97a7211fc7809c201fa0
Joel Gunawan <joelgun14@gmail.com>
2021-12-09 10:49:55 +0700

    modified template

--- tlx/pelatnas-2021/pelatnas-1/template.cpp
+++ tlx/pelatnas-2021/pelatnas-1/template.cpp
@@ -1,14 +1,14 @@
 #include <bits/stdc++.h>
+#define endl "\n"
 #define pb push_back
-#define ins isnert
+#define ins insert
 #define fi first
 #define se second
+#define mp make_pair
 #define ll long long
-#define endl "\n"
-#define ld long double
+#define lll __int128
 using namespace std;
-int main()
-{
+int main() {
     ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
     
     return 0;
