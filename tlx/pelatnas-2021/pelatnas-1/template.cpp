73ac13216b3d39768b981a090fa7971f38d9fd6c
Joel Gunawan <joelgun14@gmail.com>
2021-12-09 10:50:11 +0700

    modified template

--- tlx/pelatnas-2021/pelatnas-1/template.cpp
+++ tlx/pelatnas-2021/pelatnas-1/template.cpp
@@ -1,12 +1,17 @@
 #include <bits/stdc++.h>
 #define endl "\n"
 #define pb push_back
-#define ins insert
+#define mp make_pair
 #define fi first
 #define se second
-#define mp make_pair
+#define lb lower_bound
+#define ub upper_bound
+#define ins insert
 #define ll long long
 #define lll __int128
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+using namespace std;
 using namespace std;
 int main() {
     ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
