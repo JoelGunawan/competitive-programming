751af66148691fd1c34e4f417b8c175597c32b44
Joel Gunawan <joelgun14@gmail.com>
2022-01-17 20:49:40 +0700

    added testing solution for problemsetting

---
+++ random/tryout-ksnk-joki-olim-2022/x-prime.cpp
@@ -0,0 +1,31 @@
+// THIS TEMPLATE IS NOT OFTEN USED AS PREPARATION FOR THE NOI/IOI
+// header file
+#include <bits/stdc++.h>
+// pragma
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+// macros
+#define endl "\n"
+#define ll long long
+#define mp make_pair
+#define ins insert
+#define lb lower_bound
+#define pb push_back
+#define ub upper_bound
+#define lll __int128
+#define fi first
+#define se second
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int cnt = 0, cur = 1;
+    while(cnt < 1000) {
+        if(cur % 10 != 0)
+            ++cnt;
+        ++cur;
+    }
+    cout << cur << endl;
+    return 0;
+}
