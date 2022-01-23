afa252a7bf08e0495f2185d985b49f8a2627ea29
Joel Gunawan <joelgun14@gmail.com>
2022-01-23 07:48:17 +0700

    added nplc-2021-trial

---
+++ contests/trial-nplc-2021/a.cpp
@@ -0,0 +1,28 @@
+// header file
+#include <bits/stdc++.h>
+// pragma
+//#pragma GCC optimize("Ofast")
+//#pragma GCC optimize("unroll-loops")
+// macros
+#define endl "\r\n"
+#define ll long long
+#define mp make_pair
+#define ins insert
+#define lb lower_bound
+#define pb push_back
+#define ub upper_bound
+#define fi first
+#define se second
+using namespace std;
+
+int main()
+{
+    //ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int n;
+    scanf("%d", &n);
+    if(n % 2 == 0)
+        printf("hello world\r\n");
+    else
+        printf("no\r\n");
+    return 0;
+}
---
+++ contests/trial-nplc-2021/b.cpp
@@ -0,0 +1,28 @@
+// THIS TEMPLATE IS NOT OFTEN USED AS PREPARATION FOR THE NOI/IOI
+// header file
+#include <bits/stdc++.h>
+// pragma
+//#pragma GCC optimize("Ofast")
+//#pragma GCC optimize("unroll-loops")
+// macros
+#define endl "\r\n"
+#define ll long long
+#define mp make_pair
+#define ins insert
+#define lb lower_bound
+#define pb push_back
+#define ub upper_bound
+//#define lll __int128
+#define fi first
+#define se second
+using namespace std;
+
+int main()
+{
+    //ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int n, t;
+    scanf("%d%d", &n, &t);
+    float x = (n * t) / 100.0;
+    printf("%d\r\n", n * t / 100);
+    return 0;
+}
