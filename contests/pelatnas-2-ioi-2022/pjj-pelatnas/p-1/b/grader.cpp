200ef40904d7955a4ec1a6ae11a093b4378ccc49
Joel Gunawan <joelgun14@gmail.com>
2022-02-02 14:12:26 +0700

    added full solve for pjj-pelatnas pelatnas-2-ioi-2022 p-1b

--- contests/pelatnas-2-ioi-2022/pjj-pelatnas/p-1/b/grader.cpp
+++ contests/pelatnas-2-ioi-2022/pjj-pelatnas/p-1/b/grader.cpp
@@ -1,11 +1,16 @@
 #include "reverse.h"
 #include <iostream>
 #include <vector>
-
+#include <bits/stdc++.h>
+using namespace std;
 int N;
 
 int main() {
   std::cin >> N;
+  /*
+  for(int i = 1; i <= N; ++i)
+    reverse_array(i);
+  */
   std::vector<std::vector<int>> operations = reverse_array(N);
   std::cout << operations.size() << "\n";
   for (std::vector<int> operation : operations) {
@@ -15,4 +20,5 @@ int main() {
     }
     std::cout << "\n";
   }
+  
 }
--- contests/pelatnas-2-ioi-2022/pjj-pelatnas/p-1/b/reverse.cpp
+++ contests/pelatnas-2-ioi-2022/pjj-pelatnas/p-1/b/reverse.cpp
@@ -1,21 +1,65 @@
 #include "reverse.h"
 #include <vector>
+#include <bits/stdc++.h>
+#define endl "\n"
+#define ll long long
+#define mp make_pair
+#define ins insert
+#define lb lower_bound
+#define pb push_back
+#define ub upper_bound
+#define lll __int128
+#define fi first
+#define se second
 using namespace std;
-vector<vector<int>> operations;
-void solve(int l, int r) {
-  if(l == r)
-    return;
-  else {
-    int mid = (l + r) / 2;
-
-  }
-}
 std::vector<std::vector<int>> reverse_array(int N) {
-  // do a DNC algo
-  // where we use part of an array and we swap half the array
-  // just do 12 point solution
-  // we need to swap 2 elements only in 1 move
-  for(int i = 2; i <= N; ++i)
-    operations.push_back({i, i, 0});
+  vector<vector<int>> operations;
+  if(N == 1)
+    return vector<vector<int>>(0);
+  if(N == 2)
+    return {{2, 2, 0}};
+  operations.pb({N / 2 + N % 2, N - 1, 0});
+  int l = N / 2, r = N;
+  while(l > 1) {
+    operations.pb({l, l + 1, r});
+    --l, --r;
+  }
+  if(N & 1) {
+    operations.pb({l, l + 1, r});
+  }
+  else {
+    operations.pb({l, l, r});
+  }
+  /*
+  int a[N + 1], cur[N + 1];
+  for(int i = 0; i <= N; ++i)
+    a[i] = i;
+  cur[0] = 0;
+  for(auto ops : operations) {
+    int idx = 1;
+    for(int i = 1; i <= ops[2]; ++i) {
+      if(i < ops[0] || i > ops[1])
+        cur[idx++] = a[i];
+    }
+    for(int i = ops[0]; i <= ops[1]; ++i)
+      cur[idx++] = a[i];
+    for(int i = ops[2] + 1; i <= N; ++i) {
+      if(i < ops[0] || i > ops[1])
+        cur[idx++] = a[i];
+    }
+    for(int i = 0; i <= N; ++i)
+      a[i] = cur[i];
+  }
+  ofstream fout;
+  fout.open("output.txt",  ios_base::app);
+  bool ans = 1;
+  for(int i = 1; i <= N; ++i)
+    if(a[i] != N - i + 1)
+      ans = 0;
+  if(ans)
+    fout << "CORRECT" << endl;
+  else
+    fout << "FALSE" << endl;
+  */
   return operations;
 }
