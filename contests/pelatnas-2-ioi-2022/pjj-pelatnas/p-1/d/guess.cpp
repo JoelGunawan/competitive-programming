c367b2b3010c2ceac79e87382e8a0898125a44b4
Joel Gunawan <joelgun14@gmail.com>
2022-02-15 15:56:49 +0700

    mend
    added cf 1142b

--- contests/pelatnas-2-ioi-2022/pjj-pelatnas/p-1/d/guess.cpp
+++ contests/pelatnas-2-ioi-2022/pjj-pelatnas/p-1/d/guess.cpp
@@ -13,77 +13,72 @@ int guess(int K) {
   // binser K berdasarkan la, ra, lb, rb
   // bisa pindah search spacenya
   // yang minimum yang bakalan dikurangin search spacenya
-  int res = 0;
-  int la = 0, lb = 0, lc = 0;
-  ++K;
-  if(K >= (NA + NB) / 2) {
-    K = NA + NB - K;
-    // cari dari belakang
-    // agak susah :(
-    int la = NA - 1, lb = NB - 1;
-    while(K > 1) {
-      int mida = la - K / 2 + 1, midb = lb - K / 2 + 1;
-      int query = ask(max(mida, 0), max(midb, 0), 0);
+  if(NC == 0) {
+    if(NA == 1e5 && NB == 1e5 && K >= NA) {
+      int res = 0, la = 0, lb = 0, query;
+      
+    }
+    else {
+      int res = 0, la = 0, lb = 0, query;
+      ++K;
+      while(K > 1) {
+        int mida = la + K / 2 - 1, midb = lb + K / 2 - 1;
+        //cout << la << " " << lb << " " << mida << " " << midb << endl;
+        query = ask(min(mida, NA), min(midb, NB), NC);
+        if(query == 0)
+          la = mida + 1;
+        else
+          lb = midb + 1;
+        K -= K / 2;
+      }
+      if(lb >= NB)
+        return la;
+      else if(la >= NA)
+        return NA + lb;
+      else {
+        query = ask(min(la, NA), min(lb, NB), NC);
+        if(query == 0)
+          return la;
+        else
+          return NA + lb;
+      }
+    }
+  }
+  else {
+    int res = 0;
+    int la = 0, lb = 0, lc = 0;
+    ++K;
+    int query;
+    while(K > 2) {
+      // compare try to compare bottom and top
+      // either way the search space will be reduced by 1/2 K
+      int mida = la + K / 3 - 1, midb = lb + K / 3 - 1, midc = lc + K / 3 - 1;
+      // for now just do all except last sub
+      query = ask(min(mida, NA), min(midb, NB), min(midc, NC));
       if(query == 0)
+        la = mida + 1;
+      else if(query == 1)
         lb = midb + 1;
       else
-        la = mida + 1;
-      K -= K / 2;
+        lc = midc + 1;
+      K -= K / 3;
     }
-    if(ask(min(la, NA), min(lb, NB), 0) == 0)
+    // compare between la and lb
+    query = ask(min(la, NA), min(lb, NB), min(lc, NC));
+    if(K != 1) {
+      if(query == 0)
+        ++la;
+      else if(query == 1)
+        ++lb;
+      else
+        ++lc;
+      query = ask(min(la, NA), min(lb, NB), min(lc, NC));
+    }
+    if(query == 0)
+      return la;
+    else if(query == 1)
       return NA + lb;
     else
-      return la;
-    return 0;
+      return NA + NB + lc;
   }
-  int query;
-  while(K > 1) {
-    // compare try to compare bottom and top
-    // either way the search space will be reduced by 1/2 K
-    int mida = la + K / 2 - 1, midb = lb + K / 2 - 1;
-    // for now just do all except last sub
-    if(ask(min(mida, NA), min(midb, NB), 0) == 0)
-      la = mida + 1;
-    else
-      lb = midb + 1;
-    K -= K / 2;
-  }
-  // compare between la and lb
-  if(lb >= NB || (la <= NA && ask(min(la, NA), min(lb, NB), 0) == 0))
-    return la;
-  else
-    return NA + lb;
-  /*
-  while(K > 2) {
-    // compare try to compare bottom and top
-    // either way the search space will be reduced by 1/2 K
-    int mida = la + K / 3 - 1, midb = lb + K / 3 - 1, midc = lc + K / 3 - 1;
-    // for now just do all except last sub
-    query = ask(min(mida, NA), min(midb, NB), min(midc, NC));
-    if(query == 0)
-      la = mida + 1;
-    else if(query == 1)
-      lb = midb + 1;
-    else
-      lc = midc + 1;
-    K -= K / 3;
-  }
-  // compare between la and lb
-  query = ask(min(la, NA), min(lb, NB), min(lc, NC));
-  if(K != 1) {
-    if(query == 0)
-      ++la;
-    else if(query == 1)
-      ++lb;
-    else
-      ++lc;
-    query = ask(min(la, NA), min(lb, NB), min(lc, NC));
-  }
-  if(query == 0)
-    return la;
-  else if(query == 1)
-    return NA + lb;
-  else
-    return NA + NB + lc;
-  */
 }
--- spoj/PALIN.cpp
+++ spoj/PALIN.cpp
@@ -23,7 +23,29 @@ int main() {
     while(t--) {
         string s;
         cin >> s;
-        
+        string t;
+        t = s;
+        sort(t.begin(), t.end());
+        if(t[0] == '9' && t[t.size() - 1] == '9') {
+            // extend the count
+            cout << '1';
+            for(int i = 0; i < t.size() - 1; ++i)
+                cout << '0';
+            cout << '1';
+            cout << endl;
+        }
+        else {
+            int carry = 1, idx = s.size() - 1;
+            while(carry > 0) {
+                if(s[idx] == '9')
+                    s[idx] = '0', carry = 1;
+                else
+                    ++s[idx], carry = 0;
+                --idx;
+            }
+            // find closest bigger than or equal to palindrome
+            
+        }
     }
     return 0;
 }
