dd45e419531503eb86d09fc3bee88b8962725b4b
Joel Gunawan <joelgun14@gmail.com>
2022-05-08 02:00:40 +0700

    modified template file to include pbds headers

--- template.cpp
+++ template.cpp
@@ -1,5 +1,7 @@
 // header file
 #include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 // pragma
 #pragma GCC optimize("Ofast")
 #pragma GCC optimize("unroll-loops")
@@ -15,6 +17,8 @@
 #define fi first
 #define se second
 using namespace std;
+using namespace __gnu_pbds;
+typedef tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_multiset;
 
 int main() {
     ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
